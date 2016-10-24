e29a1f0e5b12fb44d37a3a17aaec2255972c94f7
Live session user <sandeep.k@betsol.com>
2016-10-24 11:30:50 +0000

    Donegit add .

--- js/app.js
+++ js/app.js
@@ -79,5 +79,70 @@ app.controller('signupCtrl',['$scope','$location','$http', function($scope, $loc
 }]);
 app.controller('dashboardCtrl',['$scope','$location','$http', function($scope, $location, $http){
 
+    $scope.updateProfile = function(){
+      delete $scope.profileData["connectionRequests"];
+      delete $scope.profileData["connecions"];
+      delete $scope.profileData["suggestions"];
+        var request = $http({
+          method: "PUT",
+          url: URL + "profile/" + sessionStorage.userId,
+          data: $scope.profileData
+        });
+        request.success(function(data){
+          $scope.responseMessage = "Update successful";
+          $("#dashboardMsgModal").modal('show');
+          $scope.getProfileData();
+          //var response = angular.fromJson(data);
+        });
+        request.error(function(data){
+          console.log(data);
+        });
+    }
+    $scope.getProfileData = function(){
+      var request = $http({
+        method: "GET",
+        url: URL + "profile/" + sessionStorage.userId
+      });
+      request.success(function(data){
+        $scope.profileData=angular.fromJson(data);
+        console.log(data);
+      });
+      request.error(function(data){
+        console.log(data);
+      });
+    }
+    $scope.getProfileData();
+    $scope.sendConnectRequest = function(receiverId){
+      var request = $http({
+        method: "POST",
+        url: URL + "request/send/" + sessionStorage.userId + "/" + receiverId
+      });
+      request.success(function(data) {
+        $scope.responseMessage= "Your Request Has been Sent";
+        $("#dashboardMsgModal").modal('show');
+        $scope.getProfileData();
+      });
+      request.error(function(data){
+        console.log(data);
+      });
+    }
+    $scope.acceptRequest = function(requestId){
+      var request = $http({
+        method: "POST",
+        url: URL + "request/accept/" + requestId
+      });
+      request.success(function(data){
+        $scope.responseMessage="You are Friends now";
+        $("#dashboardMsgModal").modal('show');
+        $scope.getProfileData();
+      });
+      request.error(function(data){
+        console.log(data);
+      });
+    }
+    $scope.logout = function(){
+      sessionStorage.clear();
+      $location.path("/login");
+    }
 
 }]);
